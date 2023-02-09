7bea458d90535e31bf24929e26fced8b1b62729c
맹주영 <mjy9088@naver.com>
2023-02-09 22:04:28 +0900

    fix check_duplicate_module

--- script/check_duplicate_module.sh
+++ script/check_duplicate_module.sh
@@ -4,8 +4,9 @@ set -e
 
 find ".ft/module" -depth 1 | cut -c 12- | while IFS="." read -r module_type module_name unused
 do
-  if [ "$(find ".ft/module" -depth 1 | cut -c 12- | grep -c "^$module_type.$module_name\$\\|^$module_type.$module_name.")" -ne "1" ]; then
+  if [ "$(find ".ft/module" -depth 1 | cut -c 12- | grep -cE "^$module_type\\.$module_name\$|^$module_type\\.$module_name\\.")" -ne "1" ]; then
     echo "Error: module $module_type.$module_name is resolved multiple times"
+    find ".ft/module" -depth 1 | cut -c 12- | grep -E "^$module_type\\.$module_name\$|^$module_type\\.$module_name\\." | sort
     exit 1
   fi
 done
