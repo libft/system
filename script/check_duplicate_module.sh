3530bf0934dd8a2ef24d14e9338543d7ff1897df
Juyeong Maing <mjy9088@naver.com>
2023-02-09 19:55:59 +0900

    WIP: add validator

--- script/check_duplicate_module.sh
+++ script/check_duplicate_module.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-
+find ".ft/module" -depth 1 | cut -c 12- | while IFS="." read -r module_type module_name unused
+do
+  if [ "$(find ".ft/module" -depth 1 | cut -c 12- | grep -c "^$module_type.$module_name\$\\|^$module_type.$module_name.")" -ne "1" ]; then
+    echo "Error: module $module_type.$module_name is resolved multiple times"
+    exit 1
+  fi
+done
--- script/check_peer_dependency.sh
+++ script/check_peer_dependency.sh
@@ -2,4 +2,13 @@
 
 set -e
 
-
+find ".ft/module" -depth 1 | while IFS= read -r line
+do
+  cat "$line/ft_recursive_peer_dependencies.ft"
+done | sort | uniq | while IFS= read -r line
+do
+  if [ -z "$(find ".ft/module" -depth 1 -name "$line.*")" ]; then
+    echo "Error: peer dependency $line is not resolved."
+    exit 1
+  fi
+done
