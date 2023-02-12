f6bc24a7465d34f1401cf9a36b5be9ea18a21c64
맹주영 <mjy9088@naver.com>
2023-02-13 00:37:11 +0900

    add base path resolution

--- script/common.sh
+++ script/common.sh
@@ -1,4 +1,16 @@
-[ -d "$FT_BASE_PATH" ]
+#!/bin/sh
+
+[ -d "$FT_BASE_PATH" ] || {
+  FT_BASE_PATH=$(pwd)
+  while [ "$FT_BASE_PATH" != "/" ] && [ ! -f ".ft_base_path" ]
+  do
+    FT_BASE_PATH=$(cd "$FT_BASE_PATH" && cd .. && pwd)
+  done
+  if [ ! -f "$FT_BASE_PATH/.ft_base_path" ]; then
+    exit 1
+  fi
+  export FT_BASE_PATH
+}
 
 FT_BASE_PATH="$(cd "$FT_BASE_PATH" && pwd)"
 export FT_BASE_PATH
